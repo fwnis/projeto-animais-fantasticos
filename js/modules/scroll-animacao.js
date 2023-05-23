e8f5858da764387da373c1578d357fe5fb90f9b0
Eduardo Fanis <eduardo.fanis@hotmail.com>
2023-05-23 12:15:32 -0300

    scroll-animacao refatorado

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -1,21 +1,25 @@
-export default function initAnimaScroll() {
-  const sections = document.querySelectorAll("[data-anime='scroll']");
-  if (sections.length) {
-    const windowMetade = window.innerHeight * 0.6;
+export default class AnimaScroll {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowMetade = window.innerHeight * 0.6;
 
-    function animaScroll() {
-      sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = sectionTop - windowMetade < 0;
-        if (isSectionVisible) {
-          section.classList.add("ativo");
-        } else if (section.classList.contains("ativo")) {
-          section.classList.remove("ativo")
-        }
-      });
-    }
-    animaScroll();
+    this.animaScroll = this.animaScroll.bind(this)
+  }
 
-    window.addEventListener("scroll", animaScroll);
+  animaScroll() {
+    this.sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - this.windowMetade < 0;
+      if (isSectionVisible) {
+        section.classList.add("ativo");
+      } else if (section.classList.contains("ativo")) {
+        section.classList.remove("ativo")
+      }
+    });
+  }
+
+  init() {
+    this.animaScroll()
+    window.addEventListener("scroll", this.animaScroll);
   }
 }
